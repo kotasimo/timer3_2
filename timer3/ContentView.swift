7ca2740d01a7be5d0089a5d8d7401e4308ef4e5d
simo <1804Naporeon>
2026-02-22 02:01:42 +0900

    ボタン押すとこまで

--- timer3/ContentView.swift
+++ timer3/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import Combine
+
+
+enum Mode {
+    case focus
+    case rest
+}
 
 struct ContentView: View {
+    @State private var mode: Mode = .focus
+    @State private var elapsed: TimeInterval = 0
+    @State private var isRunning: Bool = false
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                mode = (mode == .focus) ? .rest : .focus
+                isRunning = (isRunning == false) ? true : false
+            } label: {
+                Text(mode == .focus ? "集中" : "休憩")
+                Text("\(Int(elapsed))")
+                    .font(.title)
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 120)
+                    .padding()
+            }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
+        .onReceive(timer) { _ in
+            if isRunning {
+                elapsed += 1
+            } else {
+                
+            }
+        }
+        
     }
 }
 
